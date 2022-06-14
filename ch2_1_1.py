356933459fadb5bce6fe106fa507f0e0bebbd7fa
Clark1945 <dmc950213@gmail.com>
2022-06-14 11:21:44 +0800

    2-4 end

--- ch2_1_1.py
+++ ch2_1_1.py
@@ -1,3 +1,4 @@
+from matplotlib.pyplot import axis
 import numpy as np
 a=np.array([1,2,3,4,5,6])
 b=np.reshape(a,(2,3))
@@ -44,3 +45,31 @@ print(np.all(a,axis=1))
 print(np.all(a,axis=1,keepdims=True))#避免結果的降維
 #np.all(a) == a.all()
 #np.all(keepdims=True) == np.all(a,keepdims=True)
+
+a=np.random.randint(10,size=(2,3)) #0~9 建立2x3的陣列
+print(a)
+print(np.any(a==1)) 
+print(np.any(a==9,axis=0))
+print(np.any(a==9,axis=1))
+print(np.any(a==9,axis=1,keepdims=True))
+print((a%7==0).any())
+b=np.random.randint(10,size=(2,3))
+print(b)
+print((a==b).any(axis=1,keepdims=True))#判斷一軸是否重複
+
+b= np.arange(20,0,-2)
+c=np.where(b<10) #符合條件的索引值
+print(b)
+print(c)
+print(b[c])
+
+a=np.arange(12).reshape(3,4)
+print(np.where(a%2==0))
+#[0,0,1,1,2,2],
+#[0,2,0,2,0,2]  ==索引值 0-0 0-2 1-0 1-2
+
+print(np.where(a%2==0,'even','odd'))
+#print(np.where(a%2==0,'even')) ERROR
+b=np.reshape(a,(3,4))
+c=b**2
+print(np.where(b%2==0,b,c))#符合條件不變，不符合用C取代
